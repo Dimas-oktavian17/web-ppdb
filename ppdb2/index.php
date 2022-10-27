c33f42fabfbed12487044d9b9cde67553cb5a797
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2022-10-27 11:50:59 +0700

    instal swipper js & create alur pendaftaran component

--- ppdb2/index.php
+++ ppdb2/index.php
@@ -17,6 +17,8 @@
   <link rel="stylesheet" href="./asset/bootstrap-footer-18/bootstrap-footer-18/css/ionicons.min.css">
   <link rel="stylesheet" href="./asset/Bootstrap/css/bootstrap.min.css" />
   <!-- end -->
+  <!-- Swipper Js -->
+  <link rel="stylesheet" href="./asset/CSS/swiper-bundle.min.css">
   <!-- Css Only -->
   <link rel="stylesheet" href="./asset/CSS/style.css" />
   <!-- end -->
@@ -179,6 +181,67 @@
     </div>
   </section>
   <!-- akhir content-->
+  <!-- alur pendaftaran -->
+  <div class="content" id="alurPendaftaran">
+    <div class="container d-flex justify-content-center align-items-center">
+      <div class="container">
+        <!-- title -->
+        <div class="col-6 py-1 text-start">
+          <h3 class="fw-bold">
+            Alur Pendaftaran
+          </h3>
+          <p style="color: rgba(0, 0, 0, 0.7);">
+            Terdapat beberapa step pendaftaran yang harus dipenuhi agar semua rangkaian pendaftaran hingga seleksi
+            berjalan dengan
+            lancar.
+          </p>
+        </div>
+        <!-- box section -->
+        <div class="col-12">
+          <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+              <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home"
+                type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile"
+                type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact"
+                type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled"
+                type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" disabled>Disabled</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled"
+                type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" disabled>Disabled</button>
+            </li>
+          </ul>
+          <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"
+              tabindex="0">
+              ...</div>
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"
+              tabindex="0">...
+            </div>
+            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab"
+              tabindex="0">...
+            </div>
+            <div class="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab"
+              tabindex="0">...
+            </div>
+            <div class="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab"
+              tabindex="0">...
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end alur pendaftaran  -->
   <!--Features content ekskul-->
   <section class="features-content" id="ekstrakurikuler">
     <div class="container align-items-center justify-content-center">
@@ -602,6 +665,7 @@
   <script src="./asset/js/gallery.js"></script>
   <script src="./asset/Bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="./asset/js/window.js"></script>
+  <script src="./asset/js/swiper-bundle.min.js"></script>
   <!--akhir script-->
 </body>
 
